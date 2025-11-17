9af6e476f50a16100227d27b575db7227077e76b
MuhRaflie <mraflieramadhan123@gmail.com>
2025-11-17 16:22:05 +0800

    Modul 1

---
+++ pertemuan1_1/mahasiswa.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Kelas Mahasiswa (Blueprint Objek Mahasiswa)
+ * Modul 1: Konsep Dasar Class & Object
+ */
+class Mahasiswa
+{
+    // Properti (Attributes) - Data yang dimiliki objek
+    public $nama;
+    public $nim;
+
+    /**
+     * Metode (Method) - Perilaku yang dapat dilakukan objek
+     * Menampilkan salam sapaan.
+     */
+    public function sayHello()
+    {
+        // Menggunakan variabel internal kelas
+        echo "Halo, saya adalah Mahasiswa " . $this->nama . ".<br>";
+        echo "NIM saya adalah " . $this->nim . ". Selamat datang di dunia OOP!";
+    }
+}
+?>
